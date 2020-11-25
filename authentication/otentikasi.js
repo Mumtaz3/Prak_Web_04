0b94463d81fa2bee8fa20a114eb5159575b1f2f4
Mumtaz3 <72653671+Mumtaz3@users.noreply.github.com>
2020-11-25 08:41:46 +0700

    Add files via upload
    Abdullah Mumtaz_0110120002_Si02

---
+++ authentication/otentikasi.js
@@ -0,0 +1,12 @@
+
+function check(form)
+    {
+     if(form.user.value == "ahmad2017" && form.sandi.value == "integrity")
+      {
+        window.open('succes.html');
+      }
+     else
+     {
+       alert("Error Password or Username")
+      }
+    }
